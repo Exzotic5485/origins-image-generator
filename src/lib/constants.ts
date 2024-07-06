c9899b97ed172aed90bdb7911df0c1307dfc56f3
Exzotic5485 <61587328+Exzotic5485@users.noreply.github.com>
2024-07-06 18:18:33 +0100

    add default origin powers

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,347 @@
 export const DEFAULT_ORIGINS: OriginData[] = [];
 
-export const DEFAULT_POWERS: [string, OriginData][] = [];
+export const DEFAULT_POWERS: [string, PowerData][] = [
+    [
+        "origins:water_breathing",
+        {
+            "name": "Gills",
+            "description": "You can breathe underwater, but not on land."
+        }
+    ],
+    [
+        "origins:aqua_affinity",
+        {
+            "name": "Aqua Affinity",
+            "description": "You may break blocks underwater as others do on land."
+        }
+    ],
+    [
+        "origins:water_vision",
+        {
+            "name": "Wet Eyes",
+            "description": "Your vision underwater is perfect."
+        }
+    ],
+    [
+        "origins:swim_speed",
+        {
+            "name": "Fins",
+            "description": "Your underwater speed is increased."
+        }
+    ],
+    [
+        "origins:like_water",
+        {
+            "name": "Like Water",
+            "description": "When underwater, you do not sink to the ground unless you want to."
+        }
+    ],
+    [
+        "origins:fragile",
+        {
+            "name": "Fragile",
+            "description": "You have 3 less hearts of health than humans."
+        }
+    ],
+    [
+        "origins:webbing",
+        {
+            "name": "Webbing",
+            "description": "When you hit an enemy in melee, they get stuck in cobweb."
+        }
+    ],
+    [
+        "origins:climbing",
+        {
+            "name": "Climbing",
+            "description": "You are able to climb up any kind of wall, not just ladders."
+        }
+    ],
+    [
+        "origins:carnivore",
+        {
+            "name": "Carnivore",
+            "description": "Your diet is restricted to meat, you can't eat vegetables."
+        }
+    ],
+    [
+        "origins:fire_immunity",
+        {
+            "name": "Fire Immunity",
+            "description": "You are immune to all types of fire damage."
+        }
+    ],
+    [
+        "origins:nether_spawn",
+        {
+            "name": "Nether Inhabitant",
+            "description": "Your natural spawn will be in the Nether."
+        }
+    ],
+    [
+        "origins:burning_wrath",
+        {
+            "name": "Burning Wrath",
+            "description": "When on fire, you deal additional damage with your attacks."
+        }
+    ],
+    [
+        "origins:hotblooded",
+        {
+            "name": "Hotblooded",
+            "description": "Due to your hot body, venoms burn up, making you immune to poison and hunger status effects."
+        }
+    ],
+    [
+        "origins:water_vulnerability",
+        {
+            "name": "Hydrophobia",
+            "description": "You receive damage over time while in contact with water."
+        }
+    ],
+    [
+        "origins:tailwind",
+        {
+            "name": "Tailwind",
+            "description": "You are a little bit quicker on foot than others."
+        }
+    ],
+    [
+        "origins:slow_falling",
+        {
+            "name": "Featherweight",
+            "description": "You fall as gently to the ground as a feather would, unless you sneak."
+        }
+    ],
+    [
+        "origins:vegetarian",
+        {
+            "name": "Vegetarian",
+            "description": "You can't digest any meat."
+        }
+    ],
+    [
+        "origins:fresh_air",
+        {
+            "name": "Fresh Air",
+            "description": "When sleeping, your bed needs to be at an altitude of at least 86 blocks, so you can breathe fresh air."
+        }
+    ],
+    [
+        "origins:phasing",
+        {
+            "name": "Phasing",
+            "description": "While phantomized, you can walk through solid material, except Obsidian."
+        }
+    ],
+    [
+        "origins:invisibility",
+        {
+            "name": "Invisibility",
+            "description": "While phantomized, you are invisible."
+        }
+    ],
+    [
+        "origins:hunger_over_time",
+        {
+            "name": "Fast Metabolism",
+            "description": "Being phantomized causes you to become hungry."
+        }
+    ],
+    [
+        "origins:burn_in_daylight",
+        {
+            "name": "Photoallergic",
+            "description": "You begin to burn in daylight if you are not invisible."
+        }
+    ],
+    [
+        "origins:fall_immunity",
+        {
+            "name": "Acrobatics",
+            "description": "You never take fall damage, no matter from which height you fall."
+        }
+    ],
+    [
+        "origins:sprint_jump",
+        {
+            "name": "Strong Ankles",
+            "description": "You are able to jump higher by jumping while sprinting."
+        }
+    ],
+    [
+        "origins:nine_lives",
+        {
+            "name": "Nine Lives",
+            "description": "You have 1 less heart of health than humans."
+        }
+    ],
+    [
+        "origins:cat_vision",
+        {
+            "name": "Nocturnal",
+            "description": "You can slightly see in the dark when not in water."
+        }
+    ],
+    [
+        "origins:weak_arms",
+        {
+            "name": "Weak Arms",
+            "description": "When not under the effect of a strength potion, you can only mine natural stone if there are at most 2 other natural stone blocks adjacent to it."
+        }
+    ],
+    [
+        "origins:scare_creepers",
+        {
+            "name": "Catlike Appearance",
+            "description": "Creepers are scared of you and will only explode if you attack them first."
+        }
+    ],
+    [
+        "origins:launch_into_air",
+        {
+            "name": "Gift of the Winds",
+            "description": "Every 30 seconds, you are able to launch about 20 blocks up into the air."
+        }
+    ],
+    [
+        "origins:elytra",
+        {
+            "name": "Winged",
+            "description": "You have Elytra wings without needing to equip any."
+        }
+    ],
+    [
+        "origins:light_armor",
+        {
+            "name": "Need for Mobility",
+            "description": "You can not wear any heavy armor (armor with protection values higher than chainmail)."
+        }
+    ],
+    [
+        "origins:claustrophobia",
+        {
+            "name": "Claustrophobia",
+            "description": "Being somewhere with a low ceiling for too long will weaken you and make you slower."
+        }
+    ],
+    [
+        "origins:more_kinetic_damage",
+        {
+            "name": "Brittle Bones",
+            "description": "You take more damage from falling and flying into blocks."
+        }
+    ],
+    [
+        "origins:aerial_combatant",
+        {
+            "name": "Aerial Combatant",
+            "description": "You deal substantially more damage while in Elytra flight."
+        }
+    ],
+    [
+        "origins:throw_ender_pearl",
+        {
+            "name": "Teleportation",
+            "description": "Whenever you want, you may throw an ender pearl which deals no damage, allowing you to teleport."
+        }
+    ],
+    [
+        "origins:pumpkin_hate",
+        {
+            "name": "Scared of Gourds",
+            "description": "You are afraid of pumpkins. For a good reason."
+        }
+    ],
+    [
+        "origins:extra_reach",
+        {
+            "name": "Slender Body",
+            "description": "You can reach blocks and entities further away."
+        }
+    ],
+    [
+        "origins:shulker_inventory",
+        {
+            "name": "Hoarder",
+            "description": "You have access to an additional 9 slots of inventory, which keep the items on death."
+        }
+    ],
+    [
+        "origins:natural_armor",
+        {
+            "name": "Sturdy Skin",
+            "description": "Even without wearing armor, your skin provides natural protection."
+        }
+    ],
+    [
+        "origins:more_exhaustion",
+        {
+            "name": "Large Appetite",
+            "description": "You exhaust much quicker than others, thus requiring you to eat more."
+        }
+    ],
+    [
+        "origins:no_shield",
+        {
+            "name": "Unwieldy",
+            "description": "The way your hands are formed provide no way of holding a shield upright."
+        }
+    ],
+    [
+        "origins:strong_arms",
+        {
+            "name": "Strong Arms",
+            "description": "You are strong enough to break natural stones without using a pickaxe."
+        }
+    ],
+    [
+        "origins:translucent",
+        {
+            "name": "Translucent",
+            "description": "Your skin is translucent."
+        }
+    ],
+    [
+        "origins:end_spawn",
+        {
+            "name": "End Inhabitant",
+            "description": "Your journey begins in the End."
+        }
+    ],
+    [
+        "origins:master_of_webs",
+        {
+            "name": "Master of Webs",
+            "description": "You navigate cobweb perfectly, and are able to climb in them. When you hit an enemy in melee, they get stuck in cobweb for a while. Non-arthropods stuck in cobweb will be sensed by you. You are able to craft cobweb from string."
+        }
+    ],
+    [
+        "origins:lay_eggs",
+        {
+            "name": "Oviparous",
+            "description": "Whenever you wake up in the morning, you will lay an egg."
+        }
+    ],
+    [
+        "origins:phantomize",
+        {
+            "name": "Phantom Form",
+            "description": "You can switch between human and phantom form at will, but only while you are saturated enough to sprint."
+        }
+    ],
+    [
+        "origins:like_air",
+        {
+            "name": "Like Air",
+            "description": "Modifiers to your walking speed also apply while you're airborne."
+        }
+    ],
+    [
+        "origins:velvet_paws",
+        {
+            "name": "Velvet Paws",
+            "description": "Your footsteps don't cause any vibrations which could otherwise be picked up by nearby lifeforms."
+        }
+    ]
+];
